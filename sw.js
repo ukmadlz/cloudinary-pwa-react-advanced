e50e27966830d0e2564c424d637f09340cf5c97f
Mike Elsmore <mike@elsmore.me>
2017-05-04 17:24:29 +0100

    Make the service worker a bit more intelligent and pre-fetch the larger version

--- sw.js
+++ sw.js
@@ -17,10 +17,13 @@
 
   // A new ServiceWorker has been registered
   self.addEventListener("install", function (event) {
+    console.log('Installing Service Worker');
     event.waitUntil(
       caches.delete(cacheNameStatic).then(function() {
+        console.log('Delete cache: %s', cacheNameStatic);
         return caches.open(cacheNameStatic);
       }).then(function (cache) {
+        console.log('Cache: %s', cachedUrls);
         return cache.addAll(cachedUrls);
       }).catch(function(e) {
       })
@@ -29,6 +32,7 @@
 
   // A new ServiceWorker is now active
   self.addEventListener("activate", function (event) {
+    console.log("Activate Service Worker")
     event.waitUntil(
       caches.keys()
         .then(function (cacheNames) {
@@ -45,13 +49,31 @@
 
   // Save thing to cache in process of use
   self.addEventListener("fetch", function (event) {
+    console.log('Fetch item');
     event.respondWith(
       caches.open(cacheNameStatic).then(function(cache) {
         return cache.match(event.request).then(function(response) {
+          if (event.request.url.indexOf('http://res.cloudinary.com/elsmore-me/image/upload/dpr_auto/c_thumb,g_face,h_500,w_500/c_fill,h_150,w_300/g_south_east,l_cloudinary_logo,o_70,w_100,x_10,y_10/') > -1 ) {
+            let urlArray = event.request.url.split('/');
+            let publicId = urlArray[urlArray.length-1];
+            console.log('Grab the bigger version of %s', publicId);
+            let newUrl = 'http://res.cloudinary.com/elsmore-me/image/upload/dpr_auto/g_south_east,l_cloudinary_logo,o_70,w_500,x_10,y_10/fl_progressive/' + publicId;
+            let newEventRequest = Object.assign({}, event.request);
+            newEventRequest.url = newUrl;
+            fetch(newEventRequest).then(function(newNetworkResponse) {
+              console.log('Caching larger version of %s', publicId);
+              cache.put(newEventRequest, newNetworkResponse);
+            })
+          }
           var fetchPromise = fetch(event.request).then(function(networkResponse) {
             cache.put(event.request, networkResponse.clone());
             return networkResponse;
           })
+          if(response) {
+            console.log('Use Cache Version: %s', event.request.url);
+          } else {
+            console.log('Use Network Version: %s', event.request.url);
+          }
           return response || fetchPromise;
         })
       })
